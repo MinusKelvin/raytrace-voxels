9c307e3a69255250bb879198e782569e8610e7f0
MinusKelvin <mark.carlson@minuskelvin.net>
2022-06-22 18:24:15 +1000

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-use std::num::NonZeroU32;
-
-use glam::{EulerRot, IVec3, Mat3, Vec3, Vec3A};
-use image::{EncodableLayout, Rgba};
+use glam::{EulerRot, IVec3, Mat3, Vec3};
 use rand::prelude::*;
-use rayon::prelude::*;
 use winit::dpi::PhysicalSize;
 use winit::event::{DeviceEvent, ElementState, Event, VirtualKeyCode, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::{Window, WindowBuilder};
+
+mod software;
 
 fn main() {
     let mut space = Space::new(IVec3::splat(32));
@@ -36,15 +34,17 @@ fn main() {
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
 
-    let mut state = pollster::block_on(WgpuState::new(&window));
+    let mut gpu = pollster::block_on(WgpuState::new(&window));
+
+    let mut software = software::SoftwareRaytracer::new(&gpu);
 
     let mut last_time = std::time::Instant::now();
 
     event_loop.run(move |event, _, cf| match event {
         Event::WindowEvent { event, .. } => match event {
             WindowEvent::CloseRequested => *cf = ControlFlow::Exit,
-            WindowEvent::Resized(size) => state.resize(size),
-            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => state.resize(*new_inner_size),
+            WindowEvent::Resized(size) => gpu.resize(size),
+            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => gpu.resize(*new_inner_size),
             WindowEvent::KeyboardInput { input, .. } => {
                 let state = input.state == ElementState::Pressed;
                 match input.virtual_keycode {
@@ -91,9 +91,17 @@ fn main() {
 
             camera.y += (up as i32 - down as i32) as f32 * delta * 10.0;
 
-            match state.surface.get_current_texture() {
-                Ok(frame) => render(&mut state, frame, &space, camera, yaw, pitch),
-                Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+            match gpu.surface.get_current_texture() {
+                Ok(frame) => {
+                    let view = frame.texture.create_view(&wgpu::TextureViewDescriptor::default());
+
+                    let cmd = software.render(&gpu, &view, &space, camera, yaw, pitch);
+
+                    gpu.queue.submit([cmd]);
+
+                    frame.present();
+                },
+                Err(wgpu::SurfaceError::Lost) => gpu.resize(gpu.size),
                 Err(wgpu::SurfaceError::OutOfMemory) => *cf = ControlFlow::Exit,
                 Err(_) => {}
             }
@@ -111,193 +119,12 @@ fn main() {
     })
 }
 
-fn raycast(space: &Space, from: Vec3, d: Vec3) -> Option<([f32; 3], f32, Vec3)> {
-    let step = d.signum();
-    let t_delta = step / d;
-    let fudge = (1.0 + step) / 2.0;
-    let mut t_max = t_delta * (fudge - from.fract() * step);
-    if t_max.x.is_nan() {
-        t_max.x = f32::INFINITY;
-    }
-    if t_max.y.is_nan() {
-        t_max.y = f32::INFINITY;
-    }
-    if t_max.z.is_nan() {
-        t_max.z = f32::INFINITY;
-    }
-    let mut p = from.floor().as_ivec3();
-    let step = step.as_ivec3();
-    let mut i = 0;
-    loop {
-        let t = t_max.min_element();
-        let mut f = Vec3::ZERO;
-        if t_max.x < t_max.y {
-            if t_max.x < t_max.z {
-                t_max.x += t_delta.x;
-                p.x += step.x;
-                f.x = step.x as f32;
-            } else {
-                t_max.z += t_delta.z;
-                p.z += step.z;
-                f.z = step.z as f32;
-            }
-        } else {
-            if t_max.y < t_max.z {
-                t_max.y += t_delta.y;
-                p.y += step.y;
-                f.y = step.y as f32;
-            } else {
-                t_max.z += t_delta.z;
-                p.z += step.z;
-                f.z = step.z as f32;
-            }
-        }
-
-        if let Some(color) = space.get(p)? {
-            return Some((color, t, f));
-        }
-
-        i += 1;
-        if i > 100000 {
-            dbg!(step, t_max, from, d, p);
-            std::process::exit(1);
-        }
-    }
-}
-
-fn raytrace(space: &Space, from: Vec3, d: Vec3, sun: Vec3, depth_limit: usize) -> [f32; 3] {
-    if depth_limit == 0 {
-        return [0.0; 3];
-    }
-    if let Some((mut c, t, f)) = raycast(space, from, d) {
-        let is_reflective = c == [1.0; 3];
-        let p = from + d * t;
-        let lighting = sun.dot(-f).max(0.0) / 2.0 + 0.5;
-        let shadow = raycast(space, p - f * 0.001, sun).is_none() as i32 as f32;
-        let lighting = lighting.min(shadow / 2.0 + 0.5);
-        c.iter_mut().for_each(|v| *v *= lighting);
-        if is_reflective {
-            let reflected = raytrace(
-                space,
-                p - f * 0.001,
-                d - 2.0 * d.project_onto(f),
-                sun,
-                depth_limit - 1,
-            );
-            for (v, &r) in c.iter_mut().zip(reflected.iter()) {
-                *v = *v / 2.0 + r / 2.0;
-            }
-        }
-        c
-    } else {
-        [0.0; 3]
-    }
-}
-
-fn render(
-    state: &mut WgpuState,
-    frame: wgpu::SurfaceTexture,
-    space: &Space,
-    camera: Vec3,
-    yaw: f32,
-    pitch: f32,
-) {
-    let mut encoder = state
-        .device
-        .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
-
-    let looking = glam::Mat3A::from_euler(EulerRot::YXZ, yaw, pitch, 0.0);
-
-    let halfwidth = state.size.width as f32 / 2.0;
-    let halfheight = state.size.height as f32 / 2.0;
-    let sun = Vec3::new(0.1, 1.0, 0.2);
-
-    let mut raycast_image =
-        image::ImageBuffer::<Rgba<u8>, Vec<u8>>::new(state.size.width, state.size.height);
-    raycast_image
-        .enumerate_rows_mut()
-        .par_bridge()
-        .for_each(|(_, row)| {
-            for (x, y, pixel) in row {
-                let d = Vec3::from(
-                    looking
-                        * Vec3A::new(
-                            (x as f32 - halfwidth) / halfheight,
-                            (halfheight - y as f32) / halfheight,
-                            1.0,
-                        )
-                        .normalize(),
-                );
-                let color = raytrace(space, camera, d, sun, 4);
-                *pixel = Rgba([
-                    (color[0] * 255.0) as u8,
-                    (color[1] * 255.0) as u8,
-                    (color[2] * 255.0) as u8,
-                    0xff,
-                ])
-            }
-        });
-
-    state.queue.write_texture(
-        wgpu::ImageCopyTexture {
-            texture: &state.cp_tex,
-            mip_level: 0,
-            origin: wgpu::Origin3d { x: 0, y: 0, z: 0 },
-            aspect: wgpu::TextureAspect::All,
-        },
-        raycast_image.as_bytes(),
-        wgpu::ImageDataLayout {
-            offset: 0,
-            bytes_per_row: Some(NonZeroU32::new(4 * state.size.width).unwrap()),
-            rows_per_image: None,
-        },
-        wgpu::Extent3d {
-            width: state.size.width,
-            height: state.size.height,
-            depth_or_array_layers: 1,
-        },
-    );
-
-    let view = frame
-        .texture
-        .create_view(&wgpu::TextureViewDescriptor::default());
-
-    let mut rp = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-        label: None,
-        color_attachments: &[wgpu::RenderPassColorAttachment {
-            view: &view,
-            resolve_target: None,
-            ops: wgpu::Operations {
-                load: wgpu::LoadOp::Clear(wgpu::Color::BLACK),
-                store: true,
-            },
-        }],
-        depth_stencil_attachment: None,
-    });
-
-    rp.set_pipeline(&state.cp_rp);
-    rp.set_bind_group(0, &state.cp_bind, &[]);
-    rp.draw(0..6, 0..1);
-
-    drop(rp);
-
-    state.queue.submit(std::iter::once(encoder.finish()));
-
-    frame.present();
-}
-
 struct WgpuState {
     surface: wgpu::Surface,
     config: wgpu::SurfaceConfiguration,
     device: wgpu::Device,
     queue: wgpu::Queue,
     size: PhysicalSize<u32>,
-
-    cp_tex: wgpu::Texture,
-    cp_rp: wgpu::RenderPipeline,
-    cp_bind: wgpu::BindGroup,
-    cp_bind_layout: wgpu::BindGroupLayout,
-    cp_tex_view: wgpu::TextureView,
 }
 
 impl WgpuState {
@@ -336,90 +163,12 @@ impl WgpuState {
         };
         surface.configure(&device, &config);
 
-        let cp_tex = device.create_texture(&wgpu::TextureDescriptor {
-            label: None,
-            size: wgpu::Extent3d {
-                width: size.width,
-                height: size.height,
-                depth_or_array_layers: 1,
-            },
-            mip_level_count: 1,
-            sample_count: 1,
-            dimension: wgpu::TextureDimension::D2,
-            format: wgpu::TextureFormat::Rgba8UnormSrgb,
-            usage: wgpu::TextureUsages::COPY_DST | wgpu::TextureUsages::TEXTURE_BINDING,
-        });
-        let cp_tex_view = cp_tex.create_view(&wgpu::TextureViewDescriptor::default());
-
-        let cp_bind_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-            label: None,
-            entries: &[
-                wgpu::BindGroupLayoutEntry {
-                    binding: 0,
-                    visibility: wgpu::ShaderStages::FRAGMENT,
-                    ty: wgpu::BindingType::Texture {
-                        sample_type: wgpu::TextureSampleType::Float { filterable: false },
-                        view_dimension: wgpu::TextureViewDimension::D2,
-                        multisampled: false,
-                    },
-                    count: None,
-                },
-            ],
-        });
-
-        let cp_bind = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            label: None,
-            layout: &cp_bind_layout,
-            entries: &[
-                wgpu::BindGroupEntry {
-                    binding: 0,
-                    resource: wgpu::BindingResource::TextureView(&cp_tex_view),
-                },
-            ],
-        });
-
-        let cp_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-            label: None,
-            bind_group_layouts: &[&cp_bind_layout],
-            push_constant_ranges: &[],
-        });
-
-        let copy_shader = device.create_shader_module(&wgpu::include_wgsl!("copy.wgsl"));
-
-        let cp_rp = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-            label: None,
-            layout: Some(&cp_layout),
-            vertex: wgpu::VertexState {
-                module: &copy_shader,
-                entry_point: "vertex_main",
-                buffers: &[],
-            },
-            primitive: wgpu::PrimitiveState::default(),
-            depth_stencil: None,
-            multisample: wgpu::MultisampleState::default(),
-            fragment: Some(wgpu::FragmentState {
-                module: &copy_shader,
-                entry_point: "fragment_main",
-                targets: &[wgpu::ColorTargetState {
-                    format: config.format,
-                    blend: None,
-                    write_mask: wgpu::ColorWrites::ALL,
-                }],
-            }),
-            multiview: None,
-        });
-
         Self {
             surface,
             device,
             queue,
             config,
             size,
-            cp_tex,
-            cp_rp,
-            cp_bind,
-            cp_bind_layout,
-            cp_tex_view,
         }
     }
 
@@ -429,32 +178,6 @@ impl WgpuState {
             self.config.width = new_size.width;
             self.config.height = new_size.height;
             self.surface.configure(&self.device, &self.config);
-
-            self.cp_tex = self.device.create_texture(&wgpu::TextureDescriptor {
-                label: None,
-                size: wgpu::Extent3d {
-                    width: new_size.width,
-                    height: new_size.height,
-                    depth_or_array_layers: 1,
-                },
-                mip_level_count: 1,
-                sample_count: 1,
-                dimension: wgpu::TextureDimension::D2,
-                format: wgpu::TextureFormat::Rgba8UnormSrgb,
-                usage: wgpu::TextureUsages::COPY_DST | wgpu::TextureUsages::TEXTURE_BINDING,
-            });
-            self.cp_tex_view = self.cp_tex.create_view(&wgpu::TextureViewDescriptor::default());
-
-            self.cp_bind = self.device.create_bind_group(&wgpu::BindGroupDescriptor {
-                label: None,
-                layout: &self.cp_bind_layout,
-                entries: &[
-                    wgpu::BindGroupEntry {
-                        binding: 0,
-                        resource: wgpu::BindingResource::TextureView(&self.cp_tex_view),
-                    },
-                ],
-            });
         }
     }
 }
---
+++ src/software.rs
@@ -0,0 +1,305 @@
+use std::num::NonZeroU32;
+
+use glam::{Vec3, Vec3A, EulerRot};
+use image::{EncodableLayout, Rgba};
+use rayon::prelude::*;
+use winit::dpi::PhysicalSize;
+
+use crate::{Space, WgpuState};
+
+pub struct SoftwareRaytracer {
+    tex: wgpu::Texture,
+    pipeline: wgpu::RenderPipeline,
+    bind_group: wgpu::BindGroup,
+    bind_group_layout: wgpu::BindGroupLayout,
+    tex_view: wgpu::TextureView,
+    size: PhysicalSize<u32>,
+}
+
+impl SoftwareRaytracer {
+    pub(super) fn new(gpu: &WgpuState) -> Self {
+        let tex = gpu.device.create_texture(&wgpu::TextureDescriptor {
+            label: None,
+            size: wgpu::Extent3d {
+                width: gpu.size.width,
+                height: gpu.size.height,
+                depth_or_array_layers: 1,
+            },
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: wgpu::TextureDimension::D2,
+            format: wgpu::TextureFormat::Rgba8UnormSrgb,
+            usage: wgpu::TextureUsages::COPY_DST | wgpu::TextureUsages::TEXTURE_BINDING,
+        });
+        let tex_view = tex.create_view(&wgpu::TextureViewDescriptor::default());
+
+        let bind_group_layout =
+            gpu.device
+                .create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                    label: None,
+                    entries: &[wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        ty: wgpu::BindingType::Texture {
+                            sample_type: wgpu::TextureSampleType::Float { filterable: false },
+                            view_dimension: wgpu::TextureViewDimension::D2,
+                            multisampled: false,
+                        },
+                        count: None,
+                    }],
+                });
+
+        let bind_group = gpu.device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: None,
+            layout: &bind_group_layout,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: wgpu::BindingResource::TextureView(&tex_view),
+            }],
+        });
+
+        let cp_layout = gpu
+            .device
+            .create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: None,
+                bind_group_layouts: &[&bind_group_layout],
+                push_constant_ranges: &[],
+            });
+
+        let copy_shader = gpu
+            .device
+            .create_shader_module(&wgpu::include_wgsl!("copy.wgsl"));
+
+        let pipeline = gpu
+            .device
+            .create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                label: None,
+                layout: Some(&cp_layout),
+                vertex: wgpu::VertexState {
+                    module: &copy_shader,
+                    entry_point: "vertex_main",
+                    buffers: &[],
+                },
+                primitive: wgpu::PrimitiveState::default(),
+                depth_stencil: None,
+                multisample: wgpu::MultisampleState::default(),
+                fragment: Some(wgpu::FragmentState {
+                    module: &copy_shader,
+                    entry_point: "fragment_main",
+                    targets: &[wgpu::ColorTargetState {
+                        format: gpu.config.format,
+                        blend: None,
+                        write_mask: wgpu::ColorWrites::ALL,
+                    }],
+                }),
+                multiview: None,
+            });
+
+        SoftwareRaytracer {
+            tex,
+            pipeline,
+            bind_group,
+            bind_group_layout,
+            tex_view,
+            size: gpu.size,
+        }
+    }
+
+    pub(super) fn render(
+        &mut self,
+        gpu: &WgpuState,
+        target: &wgpu::TextureView,
+        space: &Space,
+        camera: Vec3,
+        yaw: f32,
+        pitch: f32,
+    ) -> wgpu::CommandBuffer {
+        if self.size != gpu.size {
+            self.size = gpu.size;
+            self.tex = gpu.device.create_texture(&wgpu::TextureDescriptor {
+                label: None,
+                size: wgpu::Extent3d {
+                    width: gpu.size.width,
+                    height: gpu.size.height,
+                    depth_or_array_layers: 1,
+                },
+                mip_level_count: 1,
+                sample_count: 1,
+                dimension: wgpu::TextureDimension::D2,
+                format: wgpu::TextureFormat::Rgba8UnormSrgb,
+                usage: wgpu::TextureUsages::COPY_DST | wgpu::TextureUsages::TEXTURE_BINDING,
+            });
+            self.tex_view = self
+                .tex
+                .create_view(&wgpu::TextureViewDescriptor::default());
+            self.bind_group = gpu.device.create_bind_group(&wgpu::BindGroupDescriptor {
+                label: None,
+                layout: &self.bind_group_layout,
+                entries: &[wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: wgpu::BindingResource::TextureView(&self.tex_view),
+                }],
+            });
+        }
+
+        let looking = glam::Mat3A::from_euler(EulerRot::YXZ, yaw, pitch, 0.0);
+    
+        let halfwidth = self.size.width as f32 / 2.0;
+        let halfheight = self.size.height as f32 / 2.0;
+        let sun = Vec3::new(0.1, 1.0, 0.2);
+
+        let mut encoder = gpu
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+        let mut raycast_image =
+            image::ImageBuffer::<Rgba<u8>, Vec<u8>>::new(self.size.width, self.size.height);
+        raycast_image
+            .enumerate_rows_mut()
+            .par_bridge()
+            .for_each(|(_, row)| {
+                for (x, y, pixel) in row {
+                    let d = Vec3::from(
+                        looking
+                            * Vec3A::new(
+                                (x as f32 - halfwidth) / halfheight,
+                                (halfheight - y as f32) / halfheight,
+                                1.0,
+                            )
+                            .normalize(),
+                    );
+                    let color = raytrace(space, camera, d, sun, 4);
+                    *pixel = Rgba([
+                        (color[0] * 255.0) as u8,
+                        (color[1] * 255.0) as u8,
+                        (color[2] * 255.0) as u8,
+                        0xff,
+                    ])
+                }
+            });
+
+        gpu.queue.write_texture(
+            wgpu::ImageCopyTexture {
+                texture: &self.tex,
+                mip_level: 0,
+                origin: wgpu::Origin3d { x: 0, y: 0, z: 0 },
+                aspect: wgpu::TextureAspect::All,
+            },
+            raycast_image.as_bytes(),
+            wgpu::ImageDataLayout {
+                offset: 0,
+                bytes_per_row: NonZeroU32::new(4 * self.size.width),
+                rows_per_image: None,
+            },
+            wgpu::Extent3d {
+                width: self.size.width,
+                height: self.size.height,
+                depth_or_array_layers: 1,
+            },
+        );
+
+        let mut rp = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: None,
+            color_attachments: &[wgpu::RenderPassColorAttachment {
+                view: &target,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(wgpu::Color::BLACK),
+                    store: true,
+                },
+            }],
+            depth_stencil_attachment: None,
+        });
+
+        rp.set_pipeline(&self.pipeline);
+        rp.set_bind_group(0, &self.bind_group, &[]);
+        rp.draw(0..6, 0..1);
+
+        drop(rp);
+
+        encoder.finish()
+    }
+}
+
+fn raycast(space: &Space, from: Vec3, d: Vec3) -> Option<([f32; 3], f32, Vec3)> {
+    let step = d.signum();
+    let t_delta = step / d;
+    let fudge = (1.0 + step) / 2.0;
+    let mut t_max = t_delta * (fudge - from.fract() * step);
+    if t_max.x.is_nan() {
+        t_max.x = f32::INFINITY;
+    }
+    if t_max.y.is_nan() {
+        t_max.y = f32::INFINITY;
+    }
+    if t_max.z.is_nan() {
+        t_max.z = f32::INFINITY;
+    }
+    let mut p = from.floor().as_ivec3();
+    let step = step.as_ivec3();
+    let mut i = 0;
+    loop {
+        let t = t_max.min_element();
+        let mut f = Vec3::ZERO;
+        if t_max.x < t_max.y {
+            if t_max.x < t_max.z {
+                t_max.x += t_delta.x;
+                p.x += step.x;
+                f.x = step.x as f32;
+            } else {
+                t_max.z += t_delta.z;
+                p.z += step.z;
+                f.z = step.z as f32;
+            }
+        } else {
+            if t_max.y < t_max.z {
+                t_max.y += t_delta.y;
+                p.y += step.y;
+                f.y = step.y as f32;
+            } else {
+                t_max.z += t_delta.z;
+                p.z += step.z;
+                f.z = step.z as f32;
+            }
+        }
+
+        if let Some(color) = space.get(p)? {
+            return Some((color, t, f));
+        }
+
+        i += 1;
+        if i > 100000 {
+            dbg!(step, t_max, from, d, p);
+            std::process::exit(1);
+        }
+    }
+}
+
+fn raytrace(space: &Space, from: Vec3, d: Vec3, sun: Vec3, depth_limit: usize) -> [f32; 3] {
+    if depth_limit == 0 {
+        return [0.0; 3];
+    }
+    if let Some((mut c, t, f)) = raycast(space, from, d) {
+        let is_reflective = c == [1.0; 3];
+        let p = from + d * t;
+        let lighting = sun.dot(-f).max(0.0) / 2.0 + 0.5;
+        let shadow = raycast(space, p - f * 0.001, sun).is_none() as i32 as f32;
+        let lighting = lighting.min(shadow / 2.0 + 0.5);
+        c.iter_mut().for_each(|v| *v *= lighting);
+        if is_reflective {
+            let reflected = raytrace(
+                space,
+                p - f * 0.001,
+                d - 2.0 * d.project_onto(f),
+                sun,
+                depth_limit - 1,
+            );
+            for (v, &r) in c.iter_mut().zip(reflected.iter()) {
+                *v = *v / 2.0 + r / 2.0;
+            }
+        }
+        c
+    } else {
+        [0.0; 3]
+    }
+}
